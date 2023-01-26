7b1c0e690f1344f0c8f7b59e42dfe45f45b0e88a
wymcg <willem.mcg@gmail.com>
2023-01-26 15:58:03 -0500

    Fix flip?

--- vid2led/util.py
+++ vid2led/util.py
@@ -35,6 +35,8 @@ def generate_coordinate_table(width, height, serpentine=False, vertical=False, f
                     # every other column of serpentine matrices are like this
                     table[(x, y)] = (x * height) + (height - y - 1)
 
+    print(table)
+
     # flip the coordinate set if requested
     if flip_horizontal:
         # make a set to hold the coordinates we've already flipped
@@ -43,23 +45,18 @@ def generate_coordinate_table(width, height, serpentine=False, vertical=False, f
         # iterate through all keys
         for coord in table.keys():
             if coord not in swapped_coords:
-                # get the coordinate we're swapping with
                 swap_coord = (width - coord[0] - 1, coord[1])
 
-                # get old indices
-                old_origin_index = table[swap_coord]
-                old_swap_index = table[coord]
+                # swap the coords
+                table[coord], table[swap_coord] = table[swap_coord], table[coord]
 
-                # make the swap
-                table[coord] = old_swap_index
-                table[swap_coord] = old_origin_index
-
-                # add the coords to the list of swapped coords
+                # mark both coords
                 swapped_coords.add(coord)
                 swapped_coords.add(swap_coord)
 
 
-
+    print()
+    print(table)
     return table
 
 
